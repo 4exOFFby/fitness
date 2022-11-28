0d2edc6be9f7fbbc7e2cc01deb5b50917df899de
Вася Чехов <4exoff@novoeradio.by>
2022-11-28 13:27:47 +0300

    Оживил таб секции абонементы

---
+++ source/js/modules/subscriber-tab.js
@@ -0,0 +1,12 @@
+// const buttons = document.querySelectorAll('.tub-button-list__button');
+// const tabLists = document.querySelectorAll('.tub-list');
+
+// console.log(buttons);
+
+
+// buttons.map((button, index) => {
+//   button.addEventListener('click', () => {
+//     console.log(button);
+//     tabLists[index].classList.remove('tub-list--active');
+//   });
+// });
